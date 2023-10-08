5789fa82fea85874f2de36e96570fcba9fb61e7a
Madhav Goswami <madhavgoswami@Madhavs-MacBook-Air.local>
2023-10-09 03:56:41 +0530

    API_BASE_URL

--- client/src/config.js
+++ client/src/config.js
@@ -1,2 +1,2 @@
-export const API_BASE_URL = "freebluebird-api.onrender.com";
+export const API_BASE_URL = "https://freebluebird-api.onrender.com";
 // export const API_BASE_URL = "http://localhost:3001";
